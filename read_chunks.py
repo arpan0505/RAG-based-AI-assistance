9b15c950d307ada7c8ddf7d854b4ac9642ba8bff
arpan0505 <arpanneog05@gmail.com>
2025-09-06 11:58:38 +0530

    Create embeddings for all videos

--- read_chunks.py
+++ read_chunks.py
@@ -1,13 +1,44 @@
 import requests
+import os
+import json
+import pandas as pd
 
-def create_embeddings(text):
-    r = requests.post("http://localhost:11434/api/embeddings", json={
+def create_embeddings(text_list):
+    r = requests.post("http://localhost:11434/api/embed", json={
     "model":"bge-m3", 
-    "prompt":text
+    "input":text_list
 })
     
-    embedding = r.json()['embedding']
-    return embedding
+    embeddings = r.json()['embeddings']
+    return embeddings
 
-a = create_embeddings("My name is Arpan Neog.")
-print(a)
+jsons = os.listdir("jsons")
+
+my_dict = []
+chunk_id = 0
+
+for json_file in jsons:
+    with open(f"jsons/{json_file}") as f:
+        content = json.load(f)
+    print(f"Create embeddings for {json_file}")    
+    embeddings = create_embeddings([c['text'] for c in content['chunks']])
+    
+    for i,chunk in enumerate(content['chunks']):
+        chunk['chunk_id'] = chunk_id
+        chunk['embedding'] = embeddings[i]
+        chunk_id += 1
+        my_dict.append(chunk)
+    
+
+# print(my_dict)
+        
+df = pd.DataFrame.from_records(my_dict)
+print(df)
+
+
+
+
+
+
+# a = create_embeddings("My name is Arpan Neog.")
+# print(a)
