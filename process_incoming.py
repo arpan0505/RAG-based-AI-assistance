4a14c30736c14b951d626c96ddfebcbf0ebdd543
arpan0505 <arpanneog05@gmail.com>
2025-09-08 19:18:24 +0530

    create prompt for llm

--- process_incoming.py
+++ process_incoming.py
@@ -19,9 +19,28 @@ df = joblib.load("embedding.joblib")
 incoming_query = input("Enter your question: ")
 question_embedding = create_embeddings([incoming_query])[0]
 similarities = cosine_similarity(np.vstack(df['embedding']), [question_embedding]).flatten()
-print(similarities)
-top_result = 5
+# print(similarities)
+top_result = 10
 max_index = similarities.argsort()[::-1][0:top_result]
-print(max_index)
+# print(max_index)
 new_df = df.loc[max_index.flatten()]
-print(new_df[["title","number","text"]])
+# print(new_df[["title","number","text"]])
+
+prompt = f'''I am teaching SQL using a structured SQL course. Here are video subtitle chunks containing video title, video number, start time in seconds, end time in seconds, and the text at that time:
+
+{new_df[["title", "number", "start", "end", "text"]].to_json()}
+
+-----------------------------------------------------------------------
+
+"{incoming_query}"
+The user asked this question related to the video chunks. You must answer by identifying in which video and at what timestamp the relevant SQL concept is explained. Clearly guide the user to the specific video and time range. 
+
+If the user’s question is unrelated to the SQL course content, politely respond that you can only answer questions related to this SQL course.'''
+
+
+with open("prompt.txt", "w", encoding="utf-8") as f:
+    f.write(prompt)
+
+# for index, item in new_df.iterrows():
+#     print(index, item['title'], item['number']  , item['text'], item['start'], item['end'])
+   
